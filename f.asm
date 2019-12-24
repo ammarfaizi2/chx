04f5eac673c846b74cc00c358e2fd284710eaa37
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-24 18:38:24 +0700

    Clean up

--- f.asm
+++ f.asm
@@ -180,9 +180,10 @@ check_input:
 	mov rdi, [rbp - 16]
 	jne .LK
 	mov r9, 0x173f3d3b6e
-	xor r9, 0xabcdef1234
+	mov r10, 0xabcdef1234
+	xor r9, r10
 	mov rax, [rcx + 8]
-	xor rax, 0xabcdef1234
+	xor rax, r10
 	jmp .LC
 .LK:
 	xor rax, rax
