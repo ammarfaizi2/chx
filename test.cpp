2bf501c6ce89cc07b7b1eeb01d4b5fcd576e40c1
Ammar Faizi <ammarfaizi2@gmail.com>
2019-12-24 09:21:05 +0700

    Clean up

--- test.cpp
+++ test.cpp
@@ -1,15 +1,16 @@
+extern "C" {
+	extern char check_input(char *rdi);
+	extern void read_input(char *rdi);
+	extern void run_action(char *rdi);
 
-extern char check_input(char *rdi);
-extern void read_input(char *rdi);
-extern void run_action(char *rdi);
-
-int main()
-{
-	char input[1024];
-	read_input(input);
-	if (check_input(input)) {
-		run_action(input);
-		return 0;
+	int main()
+	{
+		char input[1024];
+		read_input(input);
+		if (check_input(input)) {
+			run_action(input);
+			return 0;
+		}
+		return 1;
 	}
-	return 1;
 }
